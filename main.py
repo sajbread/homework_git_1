f36b541cc01c842c25b99fdadbe9d29743558e7b
systemblue <sajidulhaq1207@gmail.com>
2026-09-18 19:56:09 +0600

    added wikipedia and bing

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ search_box = driver.find_element(By.NAME, "search_query")
 search_box.send_keys("Selenium")
 search_box.send_keys(Keys.RETURN)
 time.sleep(3)
+
+driver.get("https://www.bing.com/")
+time.sleep(2)
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
+
+driver.get("https://www.wikipedia.org/")
+time.sleep(2)
+search_box = driver.find_element(By.NAME, "search")
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
+

156447a41f766b137d2d1bd0a23e719763fee7f5
systemblue <sajidulhaq1207@gmail.com>
2026-09-18 19:54:39 +0600

    added duckduckgo and youtube

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://duckduckgo.com/")
+time.sleep(2)
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
+
+driver.get("https://www.youtube.com/")
+time.sleep(2)
+search_box = driver.find_element(By.NAME, "search_query")
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
